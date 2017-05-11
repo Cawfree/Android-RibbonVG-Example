6e3d7e7d03432b1e04958c6731b845aca7930151
Alex <cawfree@gmail.com>
2017-05-11 17:09:22 +0100

    Updated example to include MSAA.

--- app/src/main/java/io/github/cawfree/example/MainActivity.java
+++ app/src/main/java/io/github/cawfree/example/MainActivity.java
@@ -21,7 +21,9 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLDisplay;
 import javax.microedition.khronos.opengles.GL10;
 
 import io.github.cawfree.example.boilerplate.AndroidGLES20;
@@ -81,6 +83,126 @@ public class MainActivity extends AppCompatActivity {
         lInputStream.close();
     }
 
+    /** Thanks to codezjx (http://stackoverflow.com/users/3919425/codezjx). */
+    private static class MultisampleConfigChooser implements GLSurfaceView.EGLConfigChooser {
+        static private final String kTag = "GDC11";
+        @Override
+        public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display) {
+            mValue = new int[1];
+
+            // Try to find a normal multisample configuration first.
+            int[] configSpec = {
+                    EGL10.EGL_RED_SIZE, 5,
+                    EGL10.EGL_GREEN_SIZE, 6,
+                    EGL10.EGL_BLUE_SIZE, 5,
+                    EGL10.EGL_DEPTH_SIZE, 16,
+                    // Requires that setEGLContextClientVersion(2) is called on the view.
+                    EGL10.EGL_RENDERABLE_TYPE, 4 /* EGL_OPENGL_ES2_BIT */,
+                    EGL10.EGL_SAMPLE_BUFFERS, 1 /* true */,
+                    EGL10.EGL_SAMPLES, 4,
+                    EGL10.EGL_NONE
+            };
+
+            if (!egl.eglChooseConfig(display, configSpec, null, 0,
+                    mValue)) {
+                throw new IllegalArgumentException("eglChooseConfig failed");
+            }
+            int numConfigs = mValue[0];
+
+            if (numConfigs <= 0) {
+                // No normal multisampling config was found. Try to create a
+                // converage multisampling configuration, for the nVidia Tegra2.
+                // See the EGL_NV_coverage_sample documentation.
+
+                final int EGL_COVERAGE_BUFFERS_NV = 0x30E0;
+                final int EGL_COVERAGE_SAMPLES_NV = 0x30E1;
+
+                configSpec = new int[]{
+                        EGL10.EGL_RED_SIZE, 5,
+                        EGL10.EGL_GREEN_SIZE, 6,
+                        EGL10.EGL_BLUE_SIZE, 5,
+                        EGL10.EGL_DEPTH_SIZE, 16,
+                        EGL10.EGL_RENDERABLE_TYPE, 4 /* EGL_OPENGL_ES2_BIT */,
+                        EGL_COVERAGE_BUFFERS_NV, 1 /* true */,
+                        EGL_COVERAGE_SAMPLES_NV, 2,  // always 5 in practice on tegra 2
+                        EGL10.EGL_NONE
+                };
+
+                if (!egl.eglChooseConfig(display, configSpec, null, 0,
+                        mValue)) {
+                    throw new IllegalArgumentException("2nd eglChooseConfig failed");
+                }
+                numConfigs = mValue[0];
+
+                if (numConfigs <= 0) {
+                    // Give up, try without multisampling.
+                    configSpec = new int[]{
+                            EGL10.EGL_RED_SIZE, 5,
+                            EGL10.EGL_GREEN_SIZE, 6,
+                            EGL10.EGL_BLUE_SIZE, 5,
+                            EGL10.EGL_DEPTH_SIZE, 16,
+                            EGL10.EGL_RENDERABLE_TYPE, 4 /* EGL_OPENGL_ES2_BIT */,
+                            EGL10.EGL_NONE
+                    };
+
+                    if (!egl.eglChooseConfig(display, configSpec, null, 0,
+                            mValue)) {
+                        throw new IllegalArgumentException("3rd eglChooseConfig failed");
+                    }
+                    numConfigs = mValue[0];
+
+                    if (numConfigs <= 0) {
+                        throw new IllegalArgumentException("No configs match configSpec");
+                    }
+                } else {
+                    mUsesCoverageAa = true;
+                }
+            }
+
+            // Get all matching configurations.
+            EGLConfig[] configs = new EGLConfig[numConfigs];
+            if (!egl.eglChooseConfig(display, configSpec, configs, numConfigs,
+                    mValue)) {
+                throw new IllegalArgumentException("data eglChooseConfig failed");
+            }
+
+            // CAUTION! eglChooseConfigs returns configs with higher bit depth
+            // first: Even though we asked for rgb565 configurations, rgb888
+            // configurations are considered to be "better" and returned first.
+            // You need to explicitly filter the data returned by eglChooseConfig!
+            int index = -1;
+            for (int i = 0; i < configs.length; ++i) {
+                if (findConfigAttrib(egl, display, configs[i], EGL10.EGL_RED_SIZE, 0) == 5) {
+                    index = i;
+                    break;
+                }
+            }
+            if (index == -1) {
+                Log.w(kTag, "Did not find sane config, using first");
+            }
+            EGLConfig config = configs.length > 0 ? configs[index] : null;
+            if (config == null) {
+                throw new IllegalArgumentException("No config chosen");
+            }
+            return config;
+        }
+
+        private int findConfigAttrib(EGL10 egl, EGLDisplay display,
+                                     EGLConfig config, int attribute, int defaultValue) {
+            if (egl.eglGetConfigAttrib(display, config, attribute, mValue)) {
+                return mValue[0];
+            }
+            return defaultValue;
+        }
+
+        public boolean usesCoverageAa() {
+            return mUsesCoverageAa;
+        }
+
+        private int[] mValue;
+        private boolean mUsesCoverageAa;
+    }
+
     /* Member Variables. */
     private GLSurfaceView     mGLSurfaceView;
     private GLVectorProgram   mGLVectorProgram;
@@ -117,6 +239,8 @@ public class MainActivity extends AppCompatActivity {
         if(MainActivity.isGLES20Supported(this)) {
             // Allocate the GLSurfaceView.
             this.mGLSurfaceView = new GLSurfaceView(this);
+            // Set the EGLConfig to use MSAA.
+            this.getSurfaceView().setEGLConfigChooser(new MultisampleConfigChooser());
             // Allocate the GLContext; this is where graphically-delegated resources are managed and persisted.
             final GLContext lGLContext = new GLContext() {
                 /* Member Variables. */
@@ -180,14 +304,13 @@ public class MainActivity extends AppCompatActivity {
                     // Draw the Circle Fill and Stroke. /** TODO: Note that these are persisted between frames! The shapes are changing, but we're not actually performing any vector computation!*/
                     this.draw(pGLES20,   this.getFillBuffer(), new float[] { 0.0f, 0.0f, 1.0f, 1.0f });
                     this.draw(pGLES20, this.getStrokeBuffer(), new float[] { 1.0f, 0.0f, 0.0f, 1.0f });
-
+                    // Reset the ModelMatrix.
+                    GLMatrix.setIdentityM(this.getModelMatrix());
 
                     // Declare the String to Draw.
                     final String lText = "Hello, world!";
                     /* Supply the colour (R, G, B, A). I want to draw in red! */
                     getGLVectorProgram().onSupplyColor(pGLES20, new float[] { 0.0f, 1.0f, 0.0f, 0.5f });
-                    // Reset the ModelMatrix.
-                    GLMatrix.setIdentityM(this.getModelMatrix());
                     // Calculate the DPI Font Scale.
                     final float lFontScale = getTrueTypeFont().getFontScale(this.getDotsPerInch(), 12.0f);
                     // Calculate the Bounding Box of the Text.
@@ -195,12 +318,8 @@ public class MainActivity extends AppCompatActivity {
                     final float lHeight    = FontGlobal.onCalculateLineHeight(getTrueTypeFont(), lFontScale, lText);
                     // Offset the Text.
                     GLMatrix.translateM(this.getModelMatrix(), (this.getScreenWidth() - lWidth) / 2, (this.getScreenHeight() - lHeight) / 2, 0);
-
-                    // Supply the Font Scale, ensure we invert the Y Axis. (TTF uses a different co-ordinate system to Ribbon.)
-                    getGLVectorProgram().onSupplyScale(pGLES20, lFontScale, -lFontScale);
-					/* Update the GLVectorProgram's World Matrices. */
-                    GLUtils.onUpdateWorldMatrices(pGLES20, getGLVectorProgram(), this);
                     // Render some text.
+                    this.draw(pGLES20, "Hello, world!", lFontScale, new float[]{ 0.0f, 1.0f, 0.0f, 1.0f });
                     this.getGLTextRenderer().onRenderText(pGLES20, this, getGLVectorProgram(), lText, lFontScale);
                     // Kill the GLBuffer, since we're rendering it then destroying it on a single frame. (Note, that since the vertices are persisted on a GPU, we only have to create the circle vertices every time the screen dimension changes.)
                     /* Bind to the GLVectorProgram. */
@@ -216,6 +335,13 @@ public class MainActivity extends AppCompatActivity {
                     pGLES20.glDrawArrays(IGL.GL_TRIANGLES, 0, (pGLBuffer.getByteBuffer().capacity() / DataUtils.BYTES_PER_FLOAT));
                     // Unbind from the GLBuffer. It's no longer available to shaders.
                     pGLBuffer.unbind(pGLES20, getGLVectorProgram());
+                }
+                /** A simplistic single-line text rendering method. */
+                private final void draw(final IGLES20 pGLES20, final String pText, final float pFontScale, final float[] pColor) {
+                    // Supply the Font Scale, ensure we invert the Y Axis. (TTF uses a different co-ordinate system to Ribbon.)
+                    getGLVectorProgram().onSupplyScale(pGLES20, pFontScale, -pFontScale);
+					/* Update the GLVectorProgram's World Matrices. */
+                    GLUtils.onUpdateWorldMatrices(pGLES20, getGLVectorProgram(), this);
                 }
                 /** Generic Overrides. */
                 @Override public final float getDotsPerInch()  { return getResources().getDisplayMetrics().densityDpi; }
